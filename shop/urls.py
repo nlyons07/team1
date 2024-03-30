72d601bea33b2c3f0172fbe8026a6b0dd4eeec78
Maria Miller <mariamiller010@gmail.com>
2024-03-30 17:48:44 -0500

    updates

--- shop/urls.py
+++ shop/urls.py
@@ -6,10 +6,10 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail,
          name='product_detail'),
+    path('', views.product_list, name='product_list'),
 
 ]
