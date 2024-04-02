6fd62c02e82a7476b74ae21a99097f9070cbe0e1
nlyons07 <nlyons@unomaha.edu>
2024-04-01 20:44:12 -0500

    python Anywhere adjustments

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +9,9 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+                                                  settings.STATIC_ROOT}),  # serve static files when deployed
 ]
 
 if settings.DEBUG:
