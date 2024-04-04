a0cd256dde52688d1ecea77123ba21bd54aed337
Maria Miller <mariamiller010@gmail.com>
2024-04-04 17:16:27 -0500

    about page

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 from cart.cart import Cart
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
