c5413808e75cc5f9ad5e5c7d1d64f02f29fe99b5
caiobrito23 <caio_jose@hotmail.com>
2024-04-28 23:03:40 -0500

    Password reset
    Password reset changes

--- shop/urls.py
+++ shop/urls.py
@@ -28,5 +28,22 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout', views.logout, name='logout'),
 
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',  # Update with your actual template path
+        email_template_name='registration/password_reset_email.html',  # Update with your actual template path
+        success_url='reset_password_done'
+    ), name='reset_password'),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'  # Update with your actual template path
+    ), name='reset_password_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html',  # Update with your actual template path
+        success_url='reset_password_complete'
+    ), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'  # Update with your actual template path
+    ), name='reset_password_complete'),
+
+
 
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
